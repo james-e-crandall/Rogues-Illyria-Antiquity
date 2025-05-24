125655bae4f707d6cfd9d93f09dec6f6fa0cfdc2
james-e-crandall <james.e.crandall@gmail.com>
2025-05-23 23:20:12 -0500

    Working

---
+++ ui/game.js
@@ -0,0 +1,21 @@
+window.UnitPortraitVisualRemaps = {
+    ...(window.UnitPortraitVisualRemaps || {}),
+    'UNIT_OIKAIDES': { //your unit type
+        unitType: 'UNIT_GALLEY',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    },
+    'UNIT_LEMBUS': { //your unit type
+        unitType: 'UNIT_GALLEY',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    },
+    'UNIT_LEMBUS_2': { //your unit type
+        unitType: 'UNIT_QUADRIREME',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    }
+}
+
+
+
+
+
+
