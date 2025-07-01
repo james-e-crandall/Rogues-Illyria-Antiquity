aeeb48bf94020594ce8519b28cbf02d64dca286b
james-e-crandall <james.e.crandall@gmail.com>
2025-07-01 18:43:40 -0500

    ROG, Tips

--- ui/game.js
+++ ui/game.js
@@ -1,14 +1,14 @@
 window.UnitPortraitVisualRemaps = {
     ...(window.UnitPortraitVisualRemaps || {}),
-    'UNIT_OIKAIDES': { //your unit type
+    'UNIT_OIKAIDES_ROG': { //your unit type
         unitType: 'UNIT_GALLEY',
         background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
     },
-    'UNIT_LEMBUS': { //your unit type
+    'UNIT_LEMBUS_ROG': { //your unit type
         unitType: 'UNIT_GALLEY',
         background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
     },
-    'UNIT_LEMBUS_2': { //your unit type
+    'UNIT_LEMBUS_ROG_2': { //your unit type
         unitType: 'UNIT_QUADRIREME',
         background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
     }
